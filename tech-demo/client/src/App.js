5a49da34146ec15c5c711d9fe78b1bb955591a7f
Jamie R <jami3507@gmail.com>
2023-09-01 16:47:43 -0500

    Basic Drag & Drop Functionality

--- tech-demo/client/src/App.js
+++ tech-demo/client/src/App.js
@@ -1,7 +1,36 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react';
+import "./App.css";
 
 function App() {
-  const [backendData, setBackendData] = useState([{}])
+  const [backendData, setBackendData] = useState([{}]);
+  const [widgets, setWidgets] = useState([]);
+
+  const removeWidget = (index) => {
+    console.log("removing widget...");
+    setWidgets(widgets.filter((_, i) => i !== index));
+  }
+
+  function handleOnDrag(e, widgetType){
+    e.dataTransfer.setData("widgetType", widgetType);
+  }
+
+  function handleOnDrop(e){
+    const widgetType = e.dataTransfer.getData("widgetType");
+    setWidgets([...widgets, widgetType]);
+  }
+
+  function handleDragOver(e){
+    e.preventDefault();
+  }
+
+  function handleTrashOnDrag(e, index) {
+    e.dataTransfer.setData("currentIndex", index);
+  }
+
+  function handleTrashOnDrop(e) {
+    console.log("current index:", e.dataTransfer.getData("currentIndex"));
+    removeWidget(parseInt(e.dataTransfer.getData("currentIndex")));
+  }
 
   useEffect(() => {
     fetch("/api").then(
@@ -14,10 +43,51 @@ function App() {
   }, []);
 
   return (
-    <div>
-
+    <div className="App">
+      <div className="widgets">
+        <div 
+          className="widget"
+          draggable
+          onDragStart={(e) => handleOnDrag(e, "Widget A")}
+        >
+          Widget A
+        </div>
+        <div 
+          className="widget"
+          draggable
+          onDragStart={(e) => handleOnDrag(e, "Widget B")}
+        >
+          Widget B
+        </div>
+        <div 
+          className="widget"
+          draggable
+          onDragStart={(e) => handleOnDrag(e, "Widget C")}
+        >
+          Widget C
+        </div>
+        <div className="page" onDrop={handleOnDrop} onDragOver={handleDragOver}>
+          {widgets.map((widget, index) => (
+            <div className="droppedWidget" draggable key={index} onDragStart={(e)=> handleTrashOnDrag(e, index)}>
+              {widget}
+            </div>
+          ))}
+        </div>
+        <div className="trash" onDrop={handleTrashOnDrop} onDragOver={handleDragOver}>
+          Trash Bin
+        </div>
+      </div>
     </div>
-  )
+    // <div>
+    //   {(typeof backendData.users === 'undefined') ? (
+    //     <p>Loading...</p>
+    //   ): (
+    //     backendData.users.map((user, i) => (
+    //       <p key={i}>{user}</p>
+    //     ))
+    //   )}
+    // </div>
+  );
 }
 
 export default App
--- tech-demo/server/server.js
+++ tech-demo/server/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
+const port = 8080;
 
 app.get('/api', (req, res) => {
+    // sending data to front end is res.json({data: data});
     res.json({"users": ["userOne", "userTwo", "userThree"]})
 });
-
-const port = 8080;
 
 app.listen(port, () => {console.log(`Server started on port ${port}`)});
