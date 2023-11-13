c2a1b0675a33bcb78d3201287b9a7cb6a9fc0c76
Alexander Resurreccion <brokengaming50@gmail.com>
2023-11-13 20:36:41 +0000

    Update ros.ts * Make use of ROS interface package * Implement service host

--- basestation-docker/server/ros.ts
+++ basestation-docker/server/ros.ts
@@ -2,8 +2,9 @@ import * as rclnodejs from 'rclnodejs';
 
 export class RosNode extends rclnodejs.Node {
     PUBLISHER: rclnodejs.Publisher<'std_msgs/msg/String'>;
+    SERVICE;
 
-    constructor(outTopic: string) {
+    constructor(outTopic: string, serviceCallback: (request, response) => void) {
         // Initalize the base class constructor
         // to create a node of name "RosNode"
         super('RosNode');
@@ -13,7 +14,10 @@ export class RosNode extends rclnodejs.Node {
         this.PUBLISHER = this.createPublisher(
             'std_msgs/msg/String', // Type of topic message, stringify JSON
             outTopic // TOPIC NAME
-        )
+        );
+
+        // Create ROS2 service server
+        this.SERVICE = this.createService('health_interface/srv/HealthReport', 'astra/core/health', serviceCallback)
     }
 
     // Do not define the type of the message, oteherwise Typescript freaks out
