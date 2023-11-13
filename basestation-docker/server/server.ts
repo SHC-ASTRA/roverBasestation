c89c8790e257cb2d8802f6aae9ea81192e6dd767
Alexander Resurreccion <brokengaming50@gmail.com>
2023-11-13 06:35:08 +0000

    Update server.ts to make use of HTTP and Express * Server now expects to host a built react app

--- basestation-docker/server/server.ts
+++ basestation-docker/server/server.ts
@@ -1,10 +1,13 @@
 /* Base Imports */
-///////////////////////////////////////
+import * as path from 'path'
+/* Application Specifci Imports */
 import * as rclnodejs from 'rclnodejs';
-//import * as express from 'express';
-import {Server, IncomingMessage, ServerResponse} from 'http';
+// HTTP
 import * as http from 'http';
-import { readFile } from 'fs';
+import {Server, IncomingMessage, ServerResponse} from 'http';
+// Express
+import * as express from 'express';
+const app = express();
 /* Custom Imports */
 ///////////////////////////////////////
 import {RosNode} from './ros'
@@ -24,25 +27,11 @@ rclnodejs.init().then(() => {
     node.spin();
 });
 
-//app.listen(port, () => {console.log(`Base Station Server started on port ${port}`)});
+app.use(
+    express.static(path.join(__dirname, "..", "react-app", "build"))
+  );
 
-var server:Server = http.createServer(
-    // HTTP server request callback
-    (req: IncomingMessage, res: ServerResponse) => {
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end('okay')
-    return
-    // Never reached
-    readFile(__dirname + req.url, (err, data) => {
-        if (err) {
-          res.writeHead(404, { 'Content-Type': 'text/html' });
-          res.end('404: File not found');
-        } else {
-          res.writeHead(200, { 'Content-Type': 'text/html' });
-          res.end(data);
-        }
-      });
-});
+var server:Server = http.createServer(app);
 
 server.listen(port, '0.0.0.0', () => {
 	console.log(`LISTENING ON ${port}`);
