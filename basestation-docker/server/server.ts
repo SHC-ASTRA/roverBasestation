c55b83cca396c2856ef5e22b28668e00913cba2e
broken-admin <brokengaming50@gmail.com>
2023-12-16 19:47:11 +0000

    Update express webserver `server.ts` + Socket.io and event handlers // ROS publisher interval // Gamepad status JSON documentation + Express endpoints for GUI button arm commands + Express endpoint for POST debug endpoint

--- basestation-docker/server/server.ts
+++ basestation-docker/server/server.ts
@@ -6,10 +6,12 @@ import * as rclnodejs from 'rclnodejs';
 import {RosNode} from './ros'
 /* HTTP */
 import * as http from 'http';
-import {Server, IncomingMessage, ServerResponse} from 'http';
+import {Server as HttpServer, IncomingMessage, ServerResponse} from 'http';
 /* Express */
 import * as express from 'express';
 const app = express();
+/* Socket.io */ 
+import { Server as SocketServer } from "socket.io";
 
 // const app = express();
 const port: number = Number(process.env.PORT) || 8000;
@@ -18,10 +20,10 @@ rclnodejs.init().then(() => {
     // Create the node
     const node = new RosNode();
     node.createTopic('astra/core/control');
-    setInterval(
+    /* setInterval(
         () => node.publishData('astra/core/control', 'Hello from the Basestation!'),
-        5000        
-    );
+        10000        
+    ); */
     node.spin();
 });
 
@@ -45,16 +47,77 @@ app.get(
     }
 );
 
-// Handle POST requests that provide an Application/JSON body
+// Gamepad status
+{/* 
+{
+    buttons: {
+        a: bool,
+        b: bool,
+        x: bool,
+        y: bool,
+        left_bumper: bool,
+        right_bumper: bool,
+        left_trigger: bool,
+        right_trigger: bool,
+        view: bool,
+        menu: bool,
+        d_up: bool,
+        d_down: bool,
+        d_left: bool,
+        d_right: bool
+    },
+    trigger_vals: {
+        left_trigger_val: float,
+        right_trigger_val: float
+    },
+    stick_vals: {
+        left_hval: float,
+        left_vval: float,
+        right_hval: float,
+        right_vval: float
+    }
+}
+*/}
+
+// Arm control API endpoint
 app.post(
     '/api/arm/control', (req, res) => {
+        // Handle ROS messaging
+        return res.send('Recieved.');
+    }
+)
+
+// POST debug endpoint
+app.post(
+    '/api/debug', (req, res) => {
         console.log(JSON.stringify(req.body));
-        // Return the provided body
+        // Return the body
         return res.send(req.body);
     }
 )
 
-var server:Server = http.createServer(app);
+// HTTP server makes use of the Express handler
+var server: HttpServer = http.createServer(app);
+
+// Create an socket connection to the HTTP server session
+const io = new SocketServer(server);
+
+// Socket connection handlers
+io.on('connection', (socket) => {
+    console.log('Websocket Connection');
+    
+    // Disconnection event handler
+    socket.on('disconnect', () => {
+      console.log('Websocket Disconnection.');
+    });
+
+    /* Other Event Handlers */
+
+    // Debug
+    socket.on('debug', (data) => {
+        console.log(`Websocket Debug: ${data}`);
+    });
+});
 
 server.listen(port, '0.0.0.0', () => {
 	console.log(`LISTENING ON ${port}`);
