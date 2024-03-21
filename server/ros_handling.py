8b89a0803b783f6738b7dd3683f6bcba2acfd0c1
broken-admin <alexlresu@gmail.com>
2024-03-21 16:05:55 -0500

    Update live data handling + Helper functions for publishers and subscribers + Add callbacks for subscribers + Live data handling for API

--- server/ros_handling.py
+++ server/ros_handling.py
@@ -4,28 +4,86 @@ import rclpy
 from rclpy.node import Node
 # ROS2 standard (topic) messages
 import std_msgs.msg
+# ROS2 standard (service) srvs
+import std_srvs.srv
+
+FEEDBACK_TOPIC = "/astra/core/feedback"
+CHATTER_TOPIC = "/topic"
 
 class RosNode(Node):
+    # Dictionary of Publishers
+    publishers = {}
+    # Dictionary of Subscribers
+    subscribers = {}
+    
     def __init__(self):
-        super().__init__('test_publisher')
-        self.publisher = self.create_publisher(std_msgs.msg.String, 'flask_pub_topic', 10)
-        self.subscription = self.create_subscription(
-            std_msgs.msg.String,
-            '/topic',
-            self.chatter_callback,
-            10)
-        self.latest_message = None
+        super().__init__('astra_base')
+
+        # Basic Subscribers for testing
+        self.create_string_publisher("flask_pub_topic")
+        self.create_subscriber(CHATTER_TOPIC, self.chatter_callback)
+
+        # LiveData subscriber
+        self.create_subscriber(FEEDBACK_TOPIC, self.core_feedback_callback)
+
+        self.message_data = {}
+
+    # Subscriber Callbacks
 
     def chatter_callback(self, msg):
         print(f'chatter cb received: {msg.data}')
-        self.latest_message = msg.data
+        self.message_data[CHATTER_TOPIC] = msg.data
 
-    def publish_message(self):
-        msg = String()
-        msg.data = 'hello, world!'
-        self.publisher.publish(msg)
+    def core_feedback_callback(self, msg):
+        print(f"Recieved data from feedback topic: {msg.data}")
+        # Check if the key is defined in the dictionary
+        try:
+            self.message_data[FEEDBACK_TOPIC]
+        except:
+            self.message_data[FEEDBACK_TOPIC] = []
+        # Append to the key's list
+        self.message_data[FEEDBACK_TOPIC].append(msg.data)
 
+    ## Helper Functions
 
+    # Create a ROS publisher of String type
+    def create_string_publisher(self, topic_name):
+        self.publishers[topic_name] = self.create_publisher(
+            std_msgs.msg.String,
+            topic_name,
+            0
+        )
+        return self.publishers[topic_name]
+
+    # Publish data to a String topic
+    def publish_string_data(self, topic_name, message):
+        self.publishers[topic_name].publish(message)
+        console.log(f"Publishing data: {message}")
+
+    # Create a subscriber with a given callback
+    def create_subscriber(self, topic_name, callback):
+        self.create_subscription(
+            std_msgs.msg.String,
+            topic_name,
+            callback,
+            0
+        )
+
+    # Health Packet Service
+
+    def initalize_health_packets(self, service_callback):
+        this.core_health_service = this.create_service(
+            std_srvs.srv.Trigger,
+            '/astra/core/health',
+            service_callback
+        )
+        this.arm_health_service = this.create_service(
+            std_srvs.srv.Trigger,
+            '/astra/arm/health',
+            service_callback
+        )
+
+# Thread worker that spins the node
 def ros2_thread(node):
     print('Entering Ros2 thread')
     rclpy.spin(node)
