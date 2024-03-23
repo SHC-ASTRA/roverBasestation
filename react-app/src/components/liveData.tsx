4058848f1dc5f528bef7d1a6da4937f75970623b
broken-admin <alexlresu@gmail.com>
2024-03-23 11:38:40 -0500

    Implement basic live data

--- react-app/src/components/liveData.tsx
+++ react-app/src/components/liveData.tsx
@@ -1,20 +1,35 @@
 import React, { useEffect, useState } from 'react';
 
 export default function LiveData({
-    eventName = "/feedback/livedata", // String socketio event
+    topicName = "/astra/core/feedback", // String socketio event
     children,
     ...props
 }) {
 
-    const [data, setData] = useState("");
+    const [data, setData] = useState([]);
 
     useEffect(() => {
         // TODO: Reimplement using fetch handling to the backend api
-    });
+        let intervalValue = setInterval(() => {
+            fetch('/message_data')
+                .then((response) => response.json())
+                .then((responseData) => setData(responseData[topicName]))
+        }, 1000);
+
+        return(() => {
+            clearInterval(intervalValue);
+        })
+    }, [data]);
+
+    let listElements = [];
+
+    data.forEach((message, index) => {
+        listElements.push(<li key={index}>{message}</li>);
+      });
 
     return (
         <>
-            {data}
+            <div>{data}</div>
             {children}
         </>
     );
