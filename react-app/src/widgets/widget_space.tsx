542a9ef59ed862affe49699a26b9fb724b9e26ab
Aeyu17 <jami3507@gmail.com>
2024-04-13 13:30:44 -0500

    re-add css

--- react-app/src/widgets/widget_space.tsx
+++ react-app/src/widgets/widget_space.tsx
@@ -1,5 +1,7 @@
 // base react
 import React, { MouseEventHandler, useState, useEffect} from "react"
+import "../../node_modules/react-grid-layout/css/styles.css";
+import "../../node_modules/react-resizable/css/styles.css";
 
 import Responsive, {WidthProvider} from "react-grid-layout"
 import {LayoutItem} from "react-grid-layout"
