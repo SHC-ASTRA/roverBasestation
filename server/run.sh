a6ffd8a75077a55786a7f11d5970a5ce7b2ccfe9
Aeyu17 <jami3507@gmail.com>
2024-05-11 11:13:21 -0500

    QoL shell script + add server requirements

--- server/run.sh
+++ server/run.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-source /opt/ros/humble/setup.bash
+if ! source /opt/ros/humble/setup.bash; then
+    source ~/ros2-humble/install/local_setup.bash
+fi
+
 cd ../react-app
-yarn build
-cd ../server
-python3 -m flask run --no-reload --host=0.0.0.0
+if yarn build; then
+    cd ../server
+    if python3 -m flask run --no-reload --host=0.0.0.0; then
+        echo "Goodbye!"
+    else
+        echo "The server did not build correctly."
+    fi
+else
+    echo "The react app did not build properly"
+    cd ../server
+fi
