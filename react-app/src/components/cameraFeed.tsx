d1958fdd7108830a87ac4f3be00fdfd1c5f213ba
Aeyu17 <jami3507@gmail.com>
2024-05-18 15:47:49 -0500

    Implement Camera Scaling and Rotation

--- react-app/src/components/cameraFeed.tsx
+++ react-app/src/components/cameraFeed.tsx
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from 'react';
 
 export default function CameraData({
     topicName,
+    defaultScale,
+    defaultRotation,
     ...props
 }) {
 
     const [imageData, setData] = useState({data:undefined});
+    const [scale, setScale] = useState(defaultScale || 180);
+    const [rotation, setRotation] = useState(defaultRotation || 0)
 
     useEffect(() => {
         socket.on(topicName, (image) => {
@@ -15,8 +19,33 @@ export default function CameraData({
 
     return (
         <>
-            <img src={`data:image/png;base64,${imageData.data}`}></img>
-            
+            <img width={`${scale}%`} height={`${scale}%`} style={{transform: `rotate(${rotation}deg)`}} src={`data:image/png;base64,${imageData.data}`}></img>
+            <div>
+                Scale: 
+                <input type='number' value={scale} 
+                    onChange={ (event) => {
+                        let val = Number(event.target.value)
+                        // Clamp value
+                        if(val < 10 || val > 200) {
+                            if(val < 10) val = 10;
+                            if(val > 200) val = 200;
+                        }
+                        setScale(val)
+                    }
+                }>
+                </input>
+            </div>
+            <div>
+                <button onClick={() => setRotation(rotation + 90)}>
+                    Left
+                </button>
+                <button onClick={() => setRotation(rotation - 90)}>
+                    Right
+                </button>
+                <button onClick={() => setRotation(rotation + 180)}>
+                    Horizontal Flip
+                </button>
+            </div>
         </>
     );
 }
