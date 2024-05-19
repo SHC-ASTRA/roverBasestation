1be5267585bbc770945c622388630dac03ba4ede
ASTRA-SHC <astra@uah.edu>
2024-05-18 23:44:12 -0500

    Begin image subscription dropdown implementation

--- react-app/src/components/cameraFeed.tsx
+++ react-app/src/components/cameraFeed.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react';
 
 export default function CameraData({
-    topicName,
+    defaultTopic,
     defaultScale,
     defaultRotation,
     ...props
 }) {
 
+    const [topic, setTopic] = useState(defaultTopic || "");
     const [imageData, setData] = useState({data:undefined});
-    const [scale, setScale] = useState(defaultScale || 180);
+    const [scale, setScale] = useState(defaultScale || 90);
     const [rotation, setRotation] = useState(defaultRotation || 0)
 
     useEffect(() => {
@@ -19,6 +20,16 @@ export default function CameraData({
 
     return (
         <>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown button
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </div>
             <img width={`${scale}%`} height={`${scale}%`} style={{transform: `rotate(${rotation}deg)`}} src={`data:image/png;base64,${imageData.data}`}></img>
             <div>
                 Scale: 
