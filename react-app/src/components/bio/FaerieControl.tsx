5313dcdccdaf6b10cc449ba6ced82ec3e9e93c05
ASTRA-SHC <astra@uah.edu>
2024-05-24 15:27:31 -0500

    Correct control endpoint for Faerie

--- react-app/src/components/bio/FaerieControl.tsx
+++ react-app/src/components/bio/FaerieControl.tsx
@@ -5,7 +5,7 @@ import Toggle from 'react-toggle';
 import "react-toggle/style.css"
 
 export const FaerieControl = ({
-    topicName = '/astra/arm/control'
+    topicName = '/astra/arm/bio/control'
     }) => {
     const [motorSpeed, setMotorSpeed] = useState<number>(0);
     const [textInput, setText] = useState<number>(0);
