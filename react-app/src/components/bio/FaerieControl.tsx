8cd2adf488983ed442f9361cc604f0856ba25e46
Alexander <brokengaming50@gmail.com>
2024-05-24 21:46:12 +0000

    FaerieControl commands reimplemented

--- react-app/src/components/bio/FaerieControl.tsx
+++ react-app/src/components/bio/FaerieControl.tsx
@@ -10,6 +10,39 @@ export const FaerieControl = ({
     const [motorSpeed, setMotorSpeed] = useState<number>(0);
     const [textInput, setText] = useState<number>(0);
 
+    const duty_cycle_command = 'faerie,ctrl,duty,'
+    const laser_command = 'faerie,ctrl,laser,'
+    const stop_command = 'faerie,stop'
+
+    function send_command_post(command, {toggleParam='off', valueParam=0}) {
+        let command_to_send;
+        
+        // STOP COMMAND
+        if(command.includes('stop')) {
+            command_to_send = stop_command;
+        }
+        // LASER COMMAND
+        else if(command.includes('laser')) {
+            command_to_send = laser_command + toggleParam;
+        }
+        // DUTY CYCLE COMMAND
+        else {
+            command_to_send = duty_cycle_command + valueParam;
+        }
+        fetch(topicName, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                command: command_to_send;
+            })
+        })
+
+        console.log(`Sent command ${command_to_send}`);
+    }
+
     return (
         <div>
             <div style={{padding: '0.7em 2em 0.7em 2em'}}>
@@ -21,16 +54,7 @@ export const FaerieControl = ({
                         value /= 100
                         setMotorSpeed(value);
                         
-                        fetch(topicName, {
-                            method: 'POST',
-                            headers: {
-                                'Accept': 'application/json',
-                                'Content-Type': 'application/json',
-                            },
-                            body: JSON.stringify({
-                                command: "faerie,ctrl,duty," + value,
-                            })
-                        })
+                        send_command_post(duty_cycle_command, {valueParam: value});
                     } 
                 }}/>
             </div>
@@ -47,31 +71,14 @@ export const FaerieControl = ({
                     value /= 200
                     setMotorSpeed(value);
                     
-                    fetch(topicName, {
-                        method: 'POST',
-                        headers: {
-                            'Accept': 'application/json',
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify({
-                            command: "faerie,ctrl,duty," + value,
-                        })
-                    })
+                    send_command_post(duty_cycle_command, {valueParam: value});
                 }}></input>
             </div>
 
             <button className="red-button" onClick={() => {
                 console.log("Stopping the motor");
-                fetch(topicName, {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        command: 'faerie,stop',
-                    })
-                })
+                
+                send_command_post(stop_command, {});
             }} style={{width: '8em', height: '4em'}}>Stop Motor</button>
 
             <div style={{paddingTop: '2em'}}>
@@ -79,16 +86,8 @@ export const FaerieControl = ({
                 <Toggle defaultChecked={false} onChange={(e) => {
                     const toggle: string = e.target.checked ? "on" : "off";
                     console.log("Setting the lasers to be " + toggle);
-                    fetch(topicName, {
-                        method: 'POST',
-                        headers: {
-                            'Accept': 'application/json',
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify({
-                            command: 'faerie,ctrl,laser,' + toggle,
-                        })
-                    })
+                    
+                    send_command_post(laser_command, {toggleParam: toggle});
                 }}/>
             </div>
         </div>
