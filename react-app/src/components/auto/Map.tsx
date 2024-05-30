1fe52e5d81037cd20296e43ab36b37c55bc19f2e
anshikasinha8 <anshika0218s@gmail.com>
2024-05-30 02:56:17 -0500

    fix map widget with pathing

--- react-app/src/components/auto/Map.tsx
+++ react-app/src/components/auto/Map.tsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import { MapContainer, TileLayer, Marker, Popup, Polyline, useMap } from 'react-leaflet'
 import "./Map.css"
-import L, { LatLngExpression, Icon } from "leaflet"
+import { LatLngExpression, Icon } from "leaflet"
 
 export const Map = ({
     topicName='/core/telemetry'
@@ -10,16 +10,14 @@ export const Map = ({
     const [gpsCoords, setGPSCoords] = useState({
         longitude: 0,
         latitude: 0
-    })
+    });
 
-    let points: LatLngExpression[] = [
-        [38.40643400834641, -110.79181336126874]
-    ]
+    const [gpsPoints, setGPSPoints] = useState<LatLngExpression[]>([]);
    
     const cluckyIcon = new Icon({
         iconUrl : "clucky.png",
         iconSize : [42, 40]
-    })
+    });
     
 
     useEffect(() => {
@@ -28,10 +26,9 @@ export const Map = ({
                 .then((response) => response.json())
                 .then((data) => {
                     if (data['gps_long'] && data['gps_lat']) {
-                        setGPSCoords({longitude: data['gps_long'], latitude: data['gps_lat']});
-                        if (points[points.length - 1][0] != gpsCoords.latitude || points[points.length - 1][1] != gpsCoords.longitude) {
-                            points.push([gpsCoords.latitude, gpsCoords.longitude]);
-                        }
+                        const newPoint = { latitude: data['gps_lat'], longitude: data['gps_long'] };
+                        setGPSCoords(newPoint);
+                        setGPSPoints(prevPoints => [...prevPoints, [newPoint.latitude, newPoint.longitude]]);
                     }    
                 })
         }, 1000);
@@ -41,54 +38,29 @@ export const Map = ({
         })
     }, []);
 
-    function ChangeView({ center, zoom }) {
+    console.log(gpsPoints);
+
+    function ChangeView({ center }) {
         const map = useMap();
-        map.setView(center, zoom)
+        map.setView(center)
 
         return null;
     }
-
-    // user location
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(
-            function (position) {
-                console.log(position);
-                setGPSCoords({
-                    longitude: position.coords.longitude,
-                    latitude: position.coords.latitude,
-                });
-            },
-            function (error) {
-                console.error("Error: " + error.code + " - " + error.message);
-            },
-            {
-                enableHighAccuracy: true,
-            });
-        }, 
-    []);
-
-    console.log(points)
-    // "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 
     return (
         <div >
             <div className='leaflet-container'>
                 <MapContainer center={[34.727, -86.639]} zoom={10} scrollWheelZoom={true} id="map" >
-                    <ChangeView center={points[points.length - 1]} zoom={15} />
+                    <ChangeView center={[gpsCoords.latitude, gpsCoords.longitude]} />
                     <TileLayer
                         url="./tiles/{z}/{x}/{y}.png"
                     />
-                    <Marker position={[gpsCoords.latitude, gpsCoords.longitude]}>
-                        <Popup>
-                            User location.
-                        </Popup>
-                    </Marker>
-                    <Marker position={points[points.length - 1]} icon={cluckyIcon}> </Marker>
-                    <Polyline positions={points} color="red"/>
+                    <Marker position={[gpsCoords.latitude, gpsCoords.longitude]} icon={cluckyIcon}> </Marker>
+                    <Polyline positions={gpsPoints} color="red"/>
                 </MapContainer>
             </div>
             
-            Current position: [ {points[points.length - 1][0]} , {points[points.length - 1][1]} ]
+            Current position: [ {gpsCoords.latitude} , {gpsCoords.longitude} ]
         </div>
     )
 }
