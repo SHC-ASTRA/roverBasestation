f5e5d71ca404d9c81938c1fa6b805b003fc38efc
Broken-Admin <alexlresu@gmail.com>
2024-10-16 03:11:21 +0000

    Remove obsolete scripts

--- scripts/build_yarn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env bash
-
-# Create a statically build production version of the
-# react webpage that will be hosted by the Flask
-# backend 
-
-cd react-app
-yarn build
--- scripts/install_node_packages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env bash
-
-# Make use of yarn to install all necessary
-# packages inside of the dynamic React frontend
-
-cd react-app
-yarn install
